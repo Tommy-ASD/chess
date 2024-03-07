47fa45fc143ed8f890d835592fd042fc502916c6
Tommy <82094450+Tommy-ASD@users.noreply.github.com>
2024-03-07 12:15:45 +0100

    developing on an extremely slow external SSD isnt a very pleasant experience

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,3 +1,35 @@
-fn main() {
-    println!("Hello, world!");
+use axum::{response::Html, routing::get, Router};
+use clap::Parser;
+use std::{collections::HashSet, net::SocketAddr, sync::Arc};
+use tokio::sync::{broadcast, Mutex};
+use tower_http::services::ServeDir;
+use tracing_subscriber::{layer::SubscriberExt, util::SubscriberInitExt};
+
+#[derive(Parser, Debug)]
+#[clap(author, version, about, long_about = None)]
+struct Args {
+    #[clap(long, short, action, default_value = "8080")]
+    port: u16,
+}
+
+async fn index() -> Html<&'static str> {
+    Html(":3")
+}
+
+pub struct AppState {}
+
+#[tokio::main]
+async fn main() {
+    let args = Args::parse();
+    let app_state = Arc::new(AppState {});
+
+    let app = Router::new()
+        .route("/", get(index))
+        .nest_service("/static", ServeDir::new("static"))
+        .with_state(app_state);
+    tokio::spawn(
+        hyper::Server::bind(&SocketAddr::from(([0, 0, 0, 0], args.port)))
+            .serve(app.into_make_service()),
+    );
+    loop {}
 }
